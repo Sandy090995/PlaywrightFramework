c5ed61f8436a526e23e619763ccfb28d4005b066
Sandip Chopkar <sandipchopkar789@gmail.com>
2024-05-10 02:23:10 +0530

    extent report

--- src/main/java/com/swaglab/factory/PlaywrightFactory.java
+++ src/main/java/com/swaglab/factory/PlaywrightFactory.java
@@ -3,6 +3,7 @@ package com.swaglab.factory;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Properties;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
@@ -18,23 +19,54 @@ public class PlaywrightFactory {
 	Page page;
 	Properties prop;
 
+	private static ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+	private static ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+	private static ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+	private static ThreadLocal<Page> tlPage = new ThreadLocal<>();
+
+	public static Playwright getPlaywright() {
+		return tlPlaywright.get();
+	}
+
+	public static Browser getBrowser() {
+		return tlBrowser.get();
+	}
+
+	public static BrowserContext getBrowserContext() {
+		return tlBrowserContext.get();
+	}
+
+	public static Page getPage() {
+		return tlPage.get();
+	}
+
 	public Page initBrowser(Properties prop) {
 		String browserName = prop.getProperty("browser").trim();
 		System.out.println("Browser name is : " + browserName);
-		playwright = Playwright.create();
+		// playwright = Playwright.create();
+		tlPlaywright.set(Playwright.create());
 
 		switch (browserName.toLowerCase()) {
 		case "chromium":
-			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			// browser = playwright.chromium().launch(new
+			// BrowserType.LaunchOptions().setHeadless(false));
+			tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)));
 			break;
 		case "firefox":
-			browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			// browser = playwright.firefox().launch(new
+			// BrowserType.LaunchOptions().setHeadless(false));
+			tlBrowser.set(getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(false)));
 			break;
 		case "safari":
-			browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			// browser = playwright.webkit().launch(new
+			// BrowserType.LaunchOptions().setHeadless(false));
+			tlBrowser.set(getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(false)));
 			break;
 		case "chrome":
-			browser = playwright.chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(false));
+			// browser = playwright.chromium().launch(new
+			// LaunchOptions().setChannel("chrome").setHeadless(false));
+			tlBrowser.set(
+					getPlaywright().chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(false)));
 			break;
 
 		default:
@@ -42,16 +74,20 @@ public class PlaywrightFactory {
 			break;
 		}
 
-		browserContext = browser.newContext();
-		page = browserContext.newPage();
-		page.navigate(prop.getProperty("url").trim());
-		return page;
+		// browserContext = browser.newContext();
+		tlBrowserContext.set(getBrowser().newContext());
+		// page = browserContext.newPage();
+		tlPage.set(getBrowserContext().newPage());
+		// page.navigate(prop.getProperty("url").trim());
+		getPage().navigate(prop.getProperty("url").trim());
+		// return page;
+		return getPage();
 	}
 
 	// this method is used to initialize the properties from config file
 	public Properties init_prop() {
 		try {
-			prop=new Properties();
+			prop = new Properties();
 			FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
 			prop.load(ip);
 		} catch (FileNotFoundException e) {
@@ -60,6 +96,12 @@ public class PlaywrightFactory {
 			f.printStackTrace();
 		}
 		return prop;
+	}
+
+	public static String takeScreenshot() {
+		String path = System.getProperty("user.dir" + "/screenshot/" + System.currentTimeMillis() + ".png");
+		getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		return path;
 	}
 
 }
---
+++ src/main/java/com/swaglab/listners/ExtentReportListner.java
@@ -0,0 +1,122 @@
+package com.swaglab.listners;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import static com.swaglab.factory.PlaywrightFactory.takeScreenshot;  //Static import
+
+public class ExtentReportListner implements ITestListener {
+
+	private static final String OUTPUT_FOLDER = "./build/";
+	private static final String FILE_NAME = "TestExecutionReport.html";
+
+	private static ExtentReports extent = init();
+	public static ThreadLocal<ExtentTest> test = new ThreadLocal<ExtentTest>();
+	private static ExtentReports extentReports;
+
+	private static ExtentReports init() {
+		Path path = Paths.get(OUTPUT_FOLDER);
+		// if directory exist?
+		if (Files.exists(path)) {
+			try {
+				Files.createDirectories(path);
+			} catch (IOException e) {
+				// fail to create directory
+				e.printStackTrace();
+			}
+		}
+
+		extentReports = new ExtentReports();
+		ExtentSparkReporter reporter = new ExtentSparkReporter(OUTPUT_FOLDER + FILE_NAME);
+		reporter.config().setReportName("Swaglab Automation Test Result");
+		extentReports.attachReporter(reporter);
+		extentReports.setSystemInfo("System", "Windows");
+		extentReports.setSystemInfo("Author", "Sandip");
+		extentReports.setSystemInfo("Client Name", "ABCD.COM");
+		// extentReports.setSystemInfo("ENV Name", System.getProperty("env"));
+
+		return extentReports;
+	}
+	
+	@Override
+	public void onStart(ITestContext context) {
+		System.out.println("Test Suite started!");
+	}
+	
+	@Override
+	public void onFinish(ITestContext context) {
+		System.out.println("Test suite is ending!");
+		extent.flush();
+		test.remove();
+	}
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+		String methodName=result.getMethod().getMethodName();
+		String qualifiedName=result.getMethod().getQualifiedName();
+		int last = qualifiedName.lastIndexOf(".");
+		int mid=qualifiedName.substring(0, last).lastIndexOf(".");
+		String className=qualifiedName.substring(mid+1, last);
+		
+		System.out.println(methodName+" started!");
+		ExtentTest extentTest=extent.createTest(result.getMethod().getMethodName(),
+				result.getMethod().getDescription());
+		extentTest.assignCategory(result.getTestContext().getSuite().getName());
+		
+		extentTest.assignCategory(className);
+		test.set(extentTest);
+		test.get().getModel().setStartTime(getTime(result.getStartMillis()));
+	}
+
+	@Override
+	public synchronized void onTestSuccess(ITestResult result) {
+		System.out.println((result.getMethod().getMethodName() + " passed!"));
+		test.get().skip(result.getThrowable(),MediaEntityBuilder.createScreenCaptureFromPath(takeScreenshot()).build());
+		test.get().pass("Test Passes");
+		test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+	}
+
+	@Override
+	public synchronized void onTestFailure(ITestResult result) {
+		System.out.println((result.getMethod().getMethodName()+" failed!"));
+		test.get().fail(result.getThrowable(),MediaEntityBuilder.createScreenCaptureFromPath(takeScreenshot()).build());
+		test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+	}
+
+	@Override
+	public synchronized void onTestSkipped(ITestResult result) {
+		System.out.println((result.getMethod().getMethodName()+ "skipped!"));
+		test.get().skip(result.getThrowable(),MediaEntityBuilder.createScreenCaptureFromPath(takeScreenshot()).build());
+		test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+	}
+
+	@Override
+	public synchronized void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		System.out.println("onTestFailedButWithinSuccessPercentage for "+ result.getMethod().getMethodName());
+	}
+
+	@Override
+	public synchronized void onTestFailedWithTimeout(ITestResult result) {
+		onTestFailure(result);
+	}
+
+	private Date getTime(long millis) {
+		Calendar calendar=Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.getTime();
+	}
+
+	
+}
